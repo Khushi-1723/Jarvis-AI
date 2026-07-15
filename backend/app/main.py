36b13434ded21143e1246214a417da2bfdbd56b0
Lenovo <babiverma1707@gmail.com>
2026-07-15 22:33:19 +0530

    Initial project setup

---
+++ backend/app/main.py
@@ -0,0 +1,9 @@
+# ==========================================
+# Project : Jarvis AI Assistant
+# File    : main.py
+# Purpose : Check that Python project is working correctly.
+# Author  : Your Name
+# ==========================================
+
+# print() function is used to display output on the screen.
+print("Hello, Jarvis AI is starting...")
