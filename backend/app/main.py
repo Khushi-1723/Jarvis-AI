596a2c48fe61fce2aaf149e6701f739ea8243aa3
Lenovo <babiverma1707@gmail.com>
2026-07-15 22:59:13 +0530

    Added basic FastAPI APIs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,78 @@
 # ==========================================
 # Project : Jarvis AI Assistant
 # File    : main.py
-# Purpose : Check that Python project is working correctly.
-# Author  : Your Name
+# Purpose : Start the FastAPI server
+# Author  : Khushi
 # ==========================================
 
-# print() function is used to display output on the screen.
-print("Hello, Jarvis AI is starting...")
+# Import the FastAPI class from the fastapi library.
+from fastapi import FastAPI
+
+# Create a FastAPI application object.
+app = FastAPI()
+
+# Create our first API (Home Route)
+@app.get("/")
+def home():
+    """
+    This function runs when someone opens:
+    http://127.0.0.1:8000/
+    """
+    
+    # Return data in JSON format.
+    return {
+        "message": "Welcome to Jarvis AI Assistant!"
+    }
+
+
+
+# Create the About API
+@app.get("/about")
+def about():
+    """
+    This API returns information about our project.
+    """
+
+    return {
+        "project": "Jarvis AI Assistant",
+        "developer": "Khushi",
+        "version": "1.0"
+    }
+
+
+
+# Create a Greeting API
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    """
+    This API greets the user by name.
+    """
+
+    return {
+        "message": f"Hello {name}, Welcome to Jarvis AI Assistant!"
+    }
+
+
+# Health Check API
+@app.get("/health")
+def health():
+    """
+    Check whether the server is running properly.
+    """
+
+    return {
+        "status": "OK",
+        "server": "Running"
+    }
+
+
+# Jarvis Version API
+@app.get("/version")
+def version():
+    """
+    Show current Jarvis version.
+    """
+
+    return {
+        "version": "1.0.0"
+    }
